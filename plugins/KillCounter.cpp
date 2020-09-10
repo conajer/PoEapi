9e0a4c8881d497162b909ed484205ff9be2d6642
conajer <conajer@outlook.com>
2020-09-10 09:34:39 -0400

    Exclude town areas. Remove oldest area when reaches the maximum area count limit.

--- plugins/KillCounter.cpp
+++ plugins/KillCounter.cpp
@@ -5,6 +5,7 @@
 using Monsters = std::unordered_set<int>;
 
 struct AreaMonsters {
+    int index;
     Monsters total;
     Monsters killed;
 };
@@ -12,14 +13,17 @@ struct AreaMonsters {
 class KillCounter : public PoEPlugin {
 public:
 
-    LocalPlayer* player;
     std::unordered_map<int, AreaMonsters*> latest_areas;
-    AreaMonsters* monsters;
+    LocalPlayer* player;
+    AreaMonsters* current_area;
     Monsters near_monsters;
+    bool in_town;
     int num_of_monsters, num_of_minions;
     int total_monsters, num_of_killed;
-
-    KillCounter() : PoEPlugin("KillCounter", "0.1"), player(nullptr), monsters(nullptr) {
+    int maximum_area_count = 9;
+    
+    KillCounter() : PoEPlugin("KillCounter", "0.1"), player(nullptr), current_area(nullptr) {
+        in_town = false;
         num_of_monsters = num_of_minions = 0;
         total_monsters = num_of_killed = 0;
     }
@@ -29,23 +33,48 @@ public:
     }
 
     void on_area_changed(AreaTemplate* world_area, int hash_code) {
+        in_town = world_area->is_town();
+        if (in_town)
+            return;
+        
         near_monsters.clear();
         if (latest_areas.find(hash_code) == latest_areas.end()) {
-            latest_areas[hash_code] = new AreaMonsters();
+            current_area = new AreaMonsters();
+            current_area->index = 0;
+            for (auto& i : latest_areas) {
+                if (i.second->index >= current_area->index)
+                    i.second->index += 1;
+                if (i.second->index >= maximum_area_count)
+                    latest_areas.erase(i.first);
+            }
+            latest_areas[hash_code] = current_area;
+        } else {
+            current_area = latest_areas[hash_code];
+            for (auto& i : latest_areas) {
+                if (i.second->index < current_area->index)
+                    i.second->index += 1;
+                if (i.second->index >= maximum_area_count)
+                    latest_areas.erase(i.first);
+            }
+            current_area->index = 0;
         }
-        monsters = latest_areas[hash_code];
+
         total_monsters = num_of_killed = 0;
+        PostThreadMessage(thread_id, WM_KILLED, num_of_killed, total_monsters);
     }
 
     void on_entity_changed(EntityList& entities, EntityList& removed, EntityList& added) {
         int n_monsters = 0, charges = 0, n_minions = 0;
         
+        if (in_town)
+            return;
+
         /* treat the removed near monsters as killed. */
         for (auto& i : removed) {
             shared_ptr<Entity>& entity = i.second;
             if (entity->is_monster) {
                 if (near_monsters.find(i.first) != near_monsters.end())
-                    monsters->killed.insert(i.first);
+                    current_area->killed.insert(i.first);
             }
         }
 
@@ -53,10 +82,10 @@ public:
         for (auto& i : entities) {
             shared_ptr<Entity>& entity = i.second;
             if (entity->is_monster) {
-                monsters->total.insert(i.first);
+                current_area->total.insert(i.first);
 
                 if (entity->is_dead()) {
-                    monsters->killed.insert(i.first);
+                    current_area->killed.insert(i.first);
                     continue;
                 }
 
@@ -84,9 +113,11 @@ public:
             PostThreadMessage(thread_id, WM_MONSTER_CHANGED, num_of_monsters, charges);
         }
 
-        if (num_of_killed != monsters->killed.size() || total_monsters != monsters->total.size()) {
-            total_monsters = monsters->total.size();
-            num_of_killed = monsters->killed.size();
+        if (num_of_killed != current_area->killed.size()
+            || total_monsters != current_area->total.size())
+        {
+            total_monsters = current_area->total.size();
+            num_of_killed = current_area->killed.size();
             PostThreadMessage(thread_id, WM_KILLED, num_of_killed, total_monsters);
         }
     }
