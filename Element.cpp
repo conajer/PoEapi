f5d16d2d1e24dc3f51f93be58f8948f988be23d9
conajer <conajer@outlook.com>
2020-09-21 18:34:41 -0400

    Add methods for Element class.

--- Element.cpp
+++ Element.cpp
@@ -34,8 +34,11 @@ public:
     {
         if (offsets != &element_offsets)
             offsets->insert(element_offsets.begin(), element_offsets.end());
+        add_method(L"getChilds", this, (MethodType)&Element::get_childs);
         add_method(L"getRect", this, (MethodType)&Element::get_rect, AhkPointer);
         add_method(L"getText", this, (MethodType)&Element::get_text, AhkWStringPtr);
+        add_method(L"isHighlight", this, (MethodType)&Element::is_highlight, AhkBool);
+        add_method(L"isVisible", this, (MethodType)&Element::is_visible, AhkBool);
     }
 
     wstring& get_text() {
@@ -52,13 +55,30 @@ public:
         return parent;
     }
 
+    Element* get_child(int index) {
+        addrtype addr = read<addrtype>("child", index * 8);
+        return new Element(addr);
+    }
+
     std::vector<shared_ptr<Element>>& get_childs() {
         childs.clear();
-        for (auto addr : read_array<addrtype>("childs", 0x8)) {
+        for (auto addr : read_array<addrtype>("childs", 0x0, 0x8)) {
             if (addr)
                 childs.push_back(shared_ptr<Element>(new Element(addr)));
             else
                 childs.push_back(shared_ptr<Element>());
+        }
+
+        if (obj_ref) {
+            AhkObjRef* ahkobj_ref;
+
+            __set(L"Childs", nullptr, AhkObject, nullptr);
+            __get(L"Childs", &ahkobj_ref, AhkObject);
+            AhkObj elements(ahkobj_ref);
+            for (auto& i : childs) {
+                if (i.get() != nullptr)
+                    elements.__set(L"", (AhkObjRef*)*i, AhkObject, nullptr);
+            }
         }
 
         return childs;
