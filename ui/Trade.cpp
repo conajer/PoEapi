f65cae061ed2f85a9eead432ac96e227b197d791
conajer <conajer@outlook.com>
2020-10-29 22:27:26 -0400

    Refactor Sell and Trade class.

--- ui/Trade.cpp
+++ ui/Trade.cpp
@@ -6,19 +6,6 @@ class Trade : public Sell {
 public:
 
     Trade(addrtype address) : Sell(address) {
-    }
-
-    shared_ptr<Element>& get_sell_panel() {
-        if (is_visible()) {
-            sell_panel = get_child(std::vector<int>{3, 1, 0, 0});
-            sell_panel->get_childs();
-            __set(L"sellPanel", (AhkObjRef*)*sell_panel, AhkObject, nullptr);
-            __set(L"accept", (AhkObjRef*)*sell_panel->childs[5], AhkObject, nullptr);
-            __set(L"cancel", (AhkObjRef*)*sell_panel->childs[6], AhkObject, nullptr);
-        } else {
-            sell_panel.reset();
-        }
-
-        return sell_panel;
+        path = {3, 1, 0, 0};
     }
 };
