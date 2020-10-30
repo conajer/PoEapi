285b0a5461ac915d8ed1f6a5766e180cb3535617
conajer <conajer@outlook.com>
2020-10-29 22:28:13 -0400

    Refactor Chat class.

--- ui/Chat.cpp
+++ ui/Chat.cpp
@@ -5,17 +5,51 @@
 class Chat : public Element {
 public:
 
-    std::vector<wstring> messages;
-    std::wregex ignored_message;
+    shared_ptr<Element> messages;
+    shared_ptr<Element> last_message;
+    unsigned int index;
 
-    Chat(addrtype address) : Element(address), ignored_message(L"^[#$]") {
+    Chat(addrtype address) : Element(address) {
+        get_childs();
+        messages = get_child(std::vector<int>{1, 2, 1});
+        index = messages->child_count();
+
+        add_method(L"isOpened", this, (MethodType)&Chat::is_opened, AhkBool);
+        add_method(L"hasNext", this, (MethodType)&Chat::has_next, AhkBool);
+        add_method(L"nextMessage", this, (MethodType)&Chat::next_message, AhkWStringPtr);
     }
 
-    wstring* get_new_message() {
+    bool is_opened() {
+        return childs[3] ? childs[3]->is_visible() : false;
+    }
+
+    bool has_next() {
+        int n = messages->child_count();
+        if (n < index) {
+            if (n > 500) {
+                for (index = n - 10; index < n; ++index) {
+                    shared_ptr<Element> message = messages->get_child(index);
+                    if (message == last_message)
+                        break;
+                }
+            } else {
+                index = 0;
+            }
+        }
+
+        return n > index;
+    }
+
+    wstring* next_message() {
+        if (messages->child_count() > index) {
+            last_message = messages->get_child(index++);
+            return &last_message->get_text();
+        }
+
         return nullptr;
     }
 
-    int size() {
-        return messages.size();
+    int count() {
+        return messages->child_count();
     }
 };
