9a105a48bcfae5925b34e2aaf54bbc7661c2db58
conajer <conajer@outlook.com>
2021-10-27 14:02:44 -0400

    Fixed offsets of the inventory slots. Updated PoEapi to version 0.31.1.

--- PoEapi.c
+++ PoEapi.c
@@ -33,8 +33,8 @@ enum MessageTypes {
 };
 
 int major_version = 0;
-int minor_version = 30;
-int patch_level = 0;
+int minor_version = 31;
+int patch_level = 1;
 
 const char* supported_PoE_version = "3.16";
 
