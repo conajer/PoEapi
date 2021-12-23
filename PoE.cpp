1cf57ca9e56e2cd038016bf276688141bacfdf42
conajer <conajer@outlook.com>
2021-12-23 16:16:40 -0500

    Fixed logout() function.

--- PoE.cpp
+++ PoE.cpp
@@ -300,7 +300,9 @@ public:
         PMIB_TCPTABLE_OWNER_PID  tcp_table;
         DWORD size = 0;
 
-        OpenProcessToken(process_handle, TOKEN_ADJUST_PRIVILEGES , &token);
+        if (!OpenProcessToken(process_handle, TOKEN_ADJUST_PRIVILEGES , &token))
+            return;
+
         LookupPrivilegeValue(0, "SeDebugPrivilege", &luid);
         TOKEN_PRIVILEGES token_privileges = {1, {luid, SE_PRIVILEGE_REMOVED}};
         AdjustTokenPrivileges(token, false, &token_privileges, 0, 0, 0);
