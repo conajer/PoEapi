1fc0a7753cc9d8bc506e024e251e07db03be4bcf
conajer <conajer@outlook.com>
2021-12-25 14:41:35 -0500

    Added new function hasTab().

--- ui/Stash.cpp
+++ ui/Stash.cpp
@@ -13,9 +13,24 @@ public:
     Stash(addrtype address) : Element(address, &stash_offsets) {
         add_method(L"isOpened", (Element*)this, (MethodType)&Element::is_visible, AhkBool);
         add_method(L"activeTabIndex", this, (MethodType)&Stash::active_tab_index);
+        add_method(L"hasTab", this, (MethodType)&Stash::has_tab, AhkBool, ParamList{AhkWString});
     }
 
     int active_tab_index() {
         return read<int>("tabs", "active_tab_index");
+    }
+
+    bool has_tab(const wchar_t* name) {
+        shared_ptr<Element> tabs = get_child(std::vector<int>{0, 1, 0});
+        for (auto& i : tabs->get_childs()) {
+            shared_ptr<Element> e = i->get_child(std::vector<int>{0, 1});
+            if (e) {
+                wstring& tab_name = e->get_text();
+                if (!wcsicmp(tab_name.c_str(), name))
+                    return true;
+            }
+        }
+
+        return false;
     }
 };
