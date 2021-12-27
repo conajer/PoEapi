7bf30c055227ce1dc566ec482f2070119a799a54
conajer <conajer@outlook.com>
2021-12-26 19:22:58 -0500

    Added new libraries (dwm, d3d, dcomp, etc.).

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-g++ -g -Iinclude -Llib -shared -o poeapi.dll PoETask.cpp -lahkpp -lpsapi -lwinmm -ld2d1 -ldwrite -liphlpapi -lsqlite3 -lcurl
+g++ -g -Iinclude -Llib -shared -o poeapi.dll PoETask.cpp -lahkpp -lpsapi -lgdi32 -ldwmapi -lwinmm -ld3d11 -ldcomp -ld2d1 -ldwrite -liphlpapi -lsqlite3 -lcurl
