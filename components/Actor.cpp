2e893d718cd2e44f24c355df2a672bd99a1cd58a
conajer <conajer@outlook.com>
2023-04-08 08:42:51 -0400

    Updated offsets for PoE 3.21.

--- components/Actor.cpp
+++ components/Actor.cpp
@@ -70,13 +70,13 @@ enum ActionFlags {
 };
 
 static std::map<string, int> actor_component_offsets {
-    {"action",       0x1a8},
-        {"skill",     0xb8},
-        {"target",    0xe8},
-    {"action_id",    0x208},
-    {"action_count", 0x20a},
-    {"skills",       0x690},
-    {"vaal_skills",  0x6c0},
+    {"action",       0x1b0},
+        {"skill",     0xc8},
+        {"target",    0xf8},
+    {"action_id",    0x210},
+    {"action_count", 0x212},
+    {"skills",       0x6c0},
+    {"vaal_skills",  0x6f0},
 };
 
 class Actor : public Component {
